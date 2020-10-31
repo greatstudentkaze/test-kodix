2def8113785dac498cbfadc6f82c1e818bc1b84f
Ivan Nefagin <dfyz0r@list.ru>
2020-10-31 13:22:57 +0300

    feat(Button): create Button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+
+`;
+
+const Button = () => (
+  <StyledButton>Налоговый вычет</StyledButton>
+);
+
+export default Button;
