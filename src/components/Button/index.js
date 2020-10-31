84077233dc0ce19ae319960886ac2cb30acc280c
Ivan Nefagin <dfyz0r@list.ru>
2020-10-31 13:34:03 +0300

    feat(Button): add base styles

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,7 +2,48 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
-
+  display: inline-block;
+  padding: 11px 24px;
+  
+  font: inherit;
+  font-size: 12px;
+  line-height: 16px;
+  font-weight: 500;
+  color: #ffffff;
+  text-decoration: none;
+  text-transform: none;
+  text-align: center;
+  vertical-align: middle;
+  
+  background-color: transparent;
+  border: 1px solid #ffffff;
+  border-radius: 6px;
+  outline: none;
+  cursor: pointer;
+  
+  filter: drop-shadow(0px 0px 44px #cfdae7);
+  transition: 0.15s ease-out;
+  
+  &:hover,
+  &:focus,
+  &:active {
+    color: #000000;
+    background-color: #ffffff;
+    filter: none;
+  }
+  
+  &:disabled {
+    color: #ffffff;
+    background-color: #bec5cc;
+    cursor: default;
+  }
+  
+  @media (min-width: 768px) {
+    padding: 15px 32px;
+    
+    font-size: 16px;
+    line-height: 24px;
+  }
 `;
 
 const Button = () => (
