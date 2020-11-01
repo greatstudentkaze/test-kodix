eadac52af2321bbafb4dbcf377b98cebc6706411
gsk <dfyz0r@list.ru>
2020-11-01 11:06:44 +0300

    feat(Form): add Form component

---
+++ src/components/Popup/Form/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { StyledForm, FormItem, FormLabel, FormButton } from './StyledFormComponents';
+
+import FormPayments from './FormPayments';
+import FormType from './FormType';
+import Button from '../../Button';
+
+const Form = () => (
+  <StyledForm>
+    <FormItem>
+      <FormLabel htmlFor="salary">Ваша зарплата в месяц</FormLabel>
+      <input type="text" id="salary" name="salary" placeholder="Введите данные"/>
+    </FormItem>
+    <FormPayments />
+    <FormType />
+    <Button type="submit" text="Добавить" theme="red" />
+  </StyledForm>
+);
+
+export default Form;
