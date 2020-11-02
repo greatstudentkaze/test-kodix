1c6c9a20c749ff0eb8fa3d1592255ee7efbc5885
gsk <dfyz0r@list.ru>
2020-11-02 22:22:25 +0300

    refactor: move FormPayments functional

--- src/components/Popup/Form/FormPayments/index.js
+++ src/components/Popup/Form/FormPayments/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 
-import { StyledFormPayments, FormLabel } from '../StyledFormComponents';
+import { StyledFormPayments, FormLabel, FormCheckbox } from '../StyledFormComponents';
+import { getEarlyPaymentAmount, getTaxDeductionAmount } from '../../../utils';
 
-const FormPayments = ({ earlyPayments }) => {
+const FormPayments = ({ monthSalary }) => {
+  const apartmentCost = 2000000;
+  const earlyPayments = [];
+  let taxDeductionAmount = getTaxDeductionAmount(apartmentCost);
+
+  if (monthSalary < 10000) {
+    earlyPayments.length = 0;
+  } else {
+    let i = 1;
+
+    while (taxDeductionAmount) {
+      const earlyPaymentAmount = Math.round(getEarlyPaymentAmount(monthSalary, taxDeductionAmount));
+
+      earlyPayments.push(
+        <FormCheckbox key={i + earlyPaymentAmount}>
+          <input className="visually-hidden" type="checkbox" name={`payment-${i}`} id={`payment-${i}`}
+                 value={earlyPaymentAmount}/>
+          <FormLabel htmlFor={`payment-${i}`}>{earlyPaymentAmount} рублей <span>в {i}-ый год</span></FormLabel>
+        </FormCheckbox>
+      );
+
+      taxDeductionAmount -= earlyPaymentAmount;
+      i++;
+    }
+  }
 
   return (
     <StyledFormPayments>
       <FormLabel as="legend">Итого можете внести в качестве досрочных:</FormLabel>
-      {earlyPayments.length > 0 ? earlyPayments : '0 рублей :('}
+      {earlyPayments.length > 0 ? earlyPayments : '0 рублей (Ваша зарпалат должна быть не менее 10000 рублей) :('}
     </StyledFormPayments>
   );
 };
--- src/components/Popup/Form/index.js
+++ src/components/Popup/Form/index.js
@@ -1,58 +1,24 @@
 import React, { useState } from 'react';
 
-import {StyledForm, FormItem, FormLabel, FormCheckbox} from './StyledFormComponents';
+import { StyledForm, FormItem, FormLabel } from './StyledFormComponents';
 
 import FormPayments from './FormPayments';
 import FormType from './FormType';
 import Button from '../../Button';
 
-import { getEarlyPaymentAmount, getTaxDeductionAmount, isNumber } from '../../utils';
+import { isNumber } from '../../utils';
 
 const Form = () => {
   const [monthSalary, setMonthSalary] = useState('');
-  const [earlyPayments, setEarlyPayments] = useState([]);
-
-  const apartmentCost = 2000000;
-  let taxDeductionAmount = getTaxDeductionAmount(apartmentCost);
 
   const handleSalaryChange = evt => {
     if (isNumber(evt.target.value) || !evt.target.value) {
       setMonthSalary(evt.target.value);
-
-      if (evt.target.value < 10000) {
-        setEarlyPayments([]);
-      } else {
-        const tempEarlyPayments = [];
-        let i = 1;
-
-        while (taxDeductionAmount) {
-          const earlyPaymentAmount = Math.round(getEarlyPaymentAmount(evt.target.value, taxDeductionAmount));
-
-          tempEarlyPayments.push(
-            <FormCheckbox key={i + earlyPaymentAmount}>
-              <input className="visually-hidden" type="checkbox" name={`payment-${i}`} id={`payment-${i}`}
-                     value={earlyPaymentAmount}/>
-              <FormLabel htmlFor={`payment-${i}`}>{earlyPaymentAmount} рублей <span>в {i}-ый год</span></FormLabel>
-            </FormCheckbox>
-          );
-
-          taxDeductionAmount -= earlyPaymentAmount;
-          i++;
-        }
-
-        setEarlyPayments(tempEarlyPayments)
-      }
     }
   }
 
   const handleSubmit = evt => {
     evt.preventDefault();
-
-    console.log('\n');
-    console.log('Input value: ', evt.target.value);
-    console.log('Month salary: ', monthSalary);
-    console.log('Tax Deduction: ', taxDeductionAmount);
-    console.log('Early payments: ', earlyPayments);
   }
 
   return (
@@ -61,7 +27,7 @@ const Form = () => {
         <FormLabel htmlFor="salary">Ваша зарплата в месяц</FormLabel>
         <input type="text" id="salary" name="salary" placeholder="Введите данные" value={monthSalary} onChange={handleSalaryChange}/>
       </FormItem>
-      {monthSalary && <FormPayments earlyPayments={earlyPayments} />}
+      {monthSalary && <FormPayments monthSalary={monthSalary} />}
       <FormType />
       <Button type="submit" text="Добавить" theme="red" />
     </StyledForm>
