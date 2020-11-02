ef69a5d4ff347da7004d823d55f66af6090657ce
Ivan Nefagin <dfyz0r@list.ru>
2020-11-03 01:01:08 +0300

    feat(Form): add salary input field validation

--- src/components/Popup/Form/index.js
+++ src/components/Popup/Form/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { StyledForm, FormItem, FormLabel } from './StyledFormComponents';
+import { StyledForm, FormItem, FormLabel, FormInputError } from './StyledFormComponents';
 
 import FormPayments from './FormPayments';
 import FormType from './FormType';
@@ -10,15 +10,36 @@ import { isNumber } from '../../utils';
 
 const Form = () => {
   const [monthSalary, setMonthSalary] = useState('');
+  const [isShowInputError, setIsShowInputError] = useState(false);
 
   const handleSalaryChange = evt => {
-    if (isNumber(evt.target.value) || !evt.target.value) {
-      setMonthSalary(evt.target.value);
+    const salaryInput = evt.target;
+
+    if (isNumber(salaryInput.value) || !salaryInput.value) {
+      setMonthSalary(salaryInput.value);
+
+      if (!salaryInput.value) {
+        salaryInput.classList.add('error');
+        setIsShowInputError(true);
+      } else {
+        salaryInput.classList.remove('error');
+        setIsShowInputError(false);
+      }
     }
   }
 
   const handleSubmit = evt => {
     evt.preventDefault();
+
+    const form = evt.target;
+
+    if (!monthSalary) {
+      form.salary.classList.add('error');
+      setIsShowInputError(true);
+    } else {
+      form.salary.classList.remove('error');
+      setIsShowInputError(false);
+    }
   }
 
   return (
@@ -26,6 +47,7 @@ const Form = () => {
       <FormItem>
         <FormLabel htmlFor="salary">Ваша зарплата в месяц</FormLabel>
         <input type="text" id="salary" name="salary" placeholder="Введите данные" value={monthSalary} onChange={handleSalaryChange}/>
+        {isShowInputError && <FormInputError>Поле обязательно для заполнения</FormInputError>}
       </FormItem>
       {monthSalary && <FormPayments monthSalary={monthSalary} />}
       <FormType />
