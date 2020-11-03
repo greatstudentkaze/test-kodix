5159e5e3700ad229ab3e9a34e2d5c5098de4bd24
Ivan Nefagin <dfyz0r@list.ru>
2020-11-03 11:16:15 +0300

    feat(Button): add passing all remaining props to component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import StyledButton from '../styled/StyledButton';
 
-const Button = ({ text, type, handleClick, theme }) => (
-  <StyledButton type={type} theme={theme} onClick={handleClick}>{text}</StyledButton>
+const Button = ({ text, type, handleClick, theme, ...props }) => (
+  <StyledButton type={type} theme={theme} onClick={handleClick} {...props}>{text}</StyledButton>
 );
 
 export default Button;
