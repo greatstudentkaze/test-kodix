3325bb960fba54c33e47fcd71836397c3b82c95f
Ivan Nefagin <dfyz0r@list.ru>
2020-11-03 10:33:50 +0300

    feat(utils): add formatPrice function, change toFixed to Math.round

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,14 +1,17 @@
 export const isNumber = n => !isNaN(parseFloat(n)) && isFinite(n);
 
+export const formatPrice = price =>
+  price.toLocaleString('ru-RU', {style: 'currency', currency: 'RUB', minimumFractionDigits: 0});
+
 export const getTaxDeductionAmount = apartmentCost => apartmentCost > 2000000 ? 260000 : apartmentCost * 0.13;
 
 export const getEarlyPaymentAmount = (monthSalary, taxDeduction) =>
   taxDeduction > (monthSalary * 12) * 0.13 ? (monthSalary * 12) * 0.13 : taxDeduction;
 
 export const getEarlyPayments = (monthSalary, taxDeduction, getEarlyPaymentAmount) => {
-  const maxEarlyPayment = +getEarlyPaymentAmount(monthSalary, taxDeduction).toFixed(1),
+  const maxEarlyPayment = Math.round(getEarlyPaymentAmount(monthSalary, taxDeduction)),
     maxEarlyPaymentCount = Math.trunc(taxDeduction / maxEarlyPayment),
-    lastPayment = +(taxDeduction - maxEarlyPayment * maxEarlyPaymentCount).toFixed(1);
+    lastPayment = Math.round(taxDeduction - maxEarlyPayment * maxEarlyPaymentCount);
 
   const earlyPayments = Array(maxEarlyPaymentCount);
   earlyPayments.fill(maxEarlyPayment);
